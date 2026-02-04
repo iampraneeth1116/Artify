577824a50a2db6fe4405c63d74ea9ba55c8a5657
Praneeth <iampraneeth1116@gmail.com>
2026-02-04 11:10:53 +0530

    added testimonials

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Header from '../components/Header'
 import Steps from '../components/Steps'
 import Description from '../components/Description'
+import Testimonials from '../components/Testimonials'
+
 
 const Home = () => {
   return (
@@ -9,6 +11,7 @@ const Home = () => {
         <Header/>
         <Steps/>
         <Description/>
+        <Testimonials/>
 
     </div>
   )
