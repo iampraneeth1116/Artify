997bd9971a0462ca6b9520260334dbd7796f362c
Praneeth <iampraneeth1116@gmail.com>
2026-02-04 09:20:44 +0530

    added footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Result from './pages/Result';
 import BuyCredit from './pages/BuyCredit';
 import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 import { AppContext } from './context/AppContext';
 import { ToastContainer } from 'react-toastify';
 
@@ -14,7 +15,6 @@ const App = () => {
     <div className="px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-100">
       <ToastContainer position='bottom-right'/>
       <NavBar/>
-
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/result' element={<Result/>}/>
